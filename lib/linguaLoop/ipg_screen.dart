9dcf14782afce5d2cc7afbd9394ed3777b721c76
Nav71 <navdhif321@gmail.com>
2025-09-30 08:55:45 +0700

    Final Penduduk

--- lib/linguaLoop/ipg_screen.dart
+++ lib/linguaLoop/ipg_screen.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:fl_chart/fl_chart.dart';
 import 'package:url_launcher/url_launcher.dart';
-import 'dart:ui';
 
 class IPGData {
   final int year;
